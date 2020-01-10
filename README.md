91b5db44d7580d5ad501e67d773af4fe1483532a
Jonathan <jonathan@wiringbits.net>
2020-01-09 21:04:56 -0700

    improve run.sh script to handle bash or NO bash

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ build docker image
 `docker build -t vivaorganica_1 -f Dockerfile .`
 
 run docker container
-`docker run -p 5000:5000 --rm -it vivaorganica_1 /bin/bash`
+`docker run -p 5000:5000 --rm -it vivaorganica_1`
 
 
 ## connect to aws
--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 docker build -t vivaorganica_1 -f Dockerfile .
-docker run -p 5000:5000 --rm -it vivaorganica_1
+
+if [ $1 == bash ]; then
+    docker run -p 5000:5000 --rm -it vivaorganica_1 /bin/bash
+else
+    docker run -p 5000:5000 --rm -it vivaorganica_1
+fi
